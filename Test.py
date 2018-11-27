aece56215b3b251a1a8291b0092cd2d7512cb1d6
Elena Polyakova <enterlina@gmail.com>
2018-11-27 11:30:31 +0300

    Refactor usability tests.

---
+++ Test.py
@@ -0,0 +1,22 @@
+# x = 16
+# y = 5
+# print(x ^ y )
+# print('x: {:b} y: {:b}'.format(x, y), x)
+# print('x >> 3 : {:b}, %x'.format(x >> 2) , x >> 2)
+# print('x >> 3 : {:b}, %x'.format(x << 2) , x << 2,'%invert', ~x)
+
+
+def s(a, *vs, b=10, с=12):
+    res = a + b + с
+    print('1', res, vs)
+    for v in vs:
+        res += v
+        print(res, v)
+    return res
+
+
+print(s(11, 0, 10))
+
+print(round(.1 + .1 + .1, 10) == round(.3, 20))
+
+print(round(.1, 10) + round(.1, 10) + round(.1, 10)  == round(.3, 1))
