6eb009379f13f52cc63f13ccdda45d7037a41312
Elena Polyakova <enterlina@gmail.com>
2018-11-27 12:26:02 +0300

    update text

--- Test.py
+++ Test.py
@@ -4,13 +4,6 @@
 # print('x: {:b} y: {:b}'.format(x, y), x)
 # print('x >> 3 : {:b}, %x'.format(x >> 2) , x >> 2)
 # print('x >> 3 : {:b}, %x'.format(x << 2) , x << 2,'%invert', ~x)
-def s(a, *vs, b=10, с=12):
-    res = a + b + с
-
-print(s(11, 0, 10))
-
-print(round(.1 + .1 + .1, 10) == round(.3, 20))
-
 
 def s(a, *vs, b=10, с=12):
     res = a + b + с
